49ab260dbd9cb59ce87e0b55287f5eb709e9edc9
Bill Watters <billywatters13@gmail.com>
2020-12-08 22:19:42 -0500

    Question with answers in a random order appears on the DOM after loading now

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
-import {fetchQuizQuestions, Difficulty, QuestionState} from './API'
+import React, { useState } from 'react';
+import { fetchQuizQuestions, Difficulty, QuestionState } from './API'
 import QuestionCard from './components/QuestionCard'
 
 const TOTAL_QUESTIONS = 10
 type AnswerObject = {
-  question: string; 
-  answer: string; 
-  correct: boolean; 
+  question: string;
+  answer: string;
+  correct: boolean;
   correctAnswer: string;
 }
 
 const App = () => {
-  const [loading, setLoading] = useState(false) 
+  const [loading, setLoading] = useState(false)
   const [questions, setQuestions] = useState<QuestionState[]>([])
   const [number, setNumber] = useState(0)
   const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
@@ -19,15 +19,15 @@ const App = () => {
   const [gameOver, setGameOver] = useState(true)
 
   const startTriva = async () => {
-    setLoading(true) 
-    setGameOver(false) 
+    setLoading(true)
+    setGameOver(false)
 
-    const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY) 
+    const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
 
     setQuestions(newQuestions)
-    setScore(0); 
+    setScore(0);
     setUserAnswers([])
-    setNumber(0) 
+    setNumber(0)
     setLoading(false)
   }
 
@@ -41,16 +41,18 @@ const App = () => {
   return (
     <div className="App">
       <h1>Quiz</h1>
-      <button className='start' onClick={startTriva}>
-        Start Game
-    </button>
-    <p className='score'> Score:</p>
-    <p> Loading Questions ...</p>
-    {/*  <QuestionCard questionNum={number + 1} totalQuestions={TOTAL_QUESTIONS} question={questions[number].question} 
-    answers={questions[number].answers} userAnswer={userAnswers ? userAnswers[number] : undefined} callback={checkAnswer} />  */}
-    
-    <button className='next' onClick={nextQuestion}> 
-      Next Question
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTriva}>
+          Start Game
+        </button>
+      ) : null}
+      {!gameOver && <p className='score'> Score:</p>}
+      {loading && <p> Loading Questions ...</p>}
+      {!loading && !gameOver && <QuestionCard questionNum={number + 1} totalQuestions={TOTAL_QUESTIONS} question={questions[number].question}
+        answers={questions[number].answers} userAnswer={userAnswers ? userAnswers[number] : undefined} callback={checkAnswer} />}
+
+      <button className='next' onClick={nextQuestion}>
+        Next Question
     </button>
     </div>
   );
